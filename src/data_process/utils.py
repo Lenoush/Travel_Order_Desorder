4dfcd3dccf1bfafd46b5c3cd8d19a25dce76228d
Lenoush05 <lenaoudjman@yahoo.fr>
2024-10-31 14:25:25 +0100

    improve dataset + readme

--- src/data_process/utils.py
+++ src/data_process/utils.py
@@ -4,16 +4,18 @@ import csv
 import random
 from typing import List
 from config import SNCF_gare
+from data.data_need import ville_sans_gare
 
 
-def get_random_word_from_file(filepath: str) -> str:
+def get_random_word_from_file() -> str:
     """
     Reads a CSV file and returns a random word from the 'LIBELLE' column.
     """
 
-    with open(filepath, "r", encoding="utf-8") as file:
+    with open(SNCF_gare, "r", encoding="utf-8") as file:
         reader = csv.DictReader(file, delimiter=";")
         words = [row["LIBELLE"] for row in reader]
+    words.extend(ville_sans_gare)
     return random.choice(words)
 
 
@@ -45,7 +47,7 @@ def replace_and_generate_response(dataset: List[str]) -> List[List[str]]:
             for word in modified_phrase.split():
                 stripped_word = word.strip(".,;!?")
                 if stripped_word in ["X", "Y", "C"]:
-                    random_word = get_random_word_from_file(SNCF_gare).lower()
+                    random_word = get_random_word_from_file().lower()
                     modified_phrase = modified_phrase.replace(word, random_word, 1)
                     if stripped_word == "X":
                         reponse[0] = random_word
@@ -70,7 +72,7 @@ def replace_and_generate_error(dataset: List[str]) -> List[List[str]]:
             for word in modified_phrase.split():
                 stripped_word = word.strip(".,;!?")
                 if stripped_word == "X":
-                    random_word = get_random_word_from_file(SNCF_gare).lower()
+                    random_word = get_random_word_from_file().lower()
                     modified_phrase = modified_phrase.replace(word, random_word, 1)
             processed_data.append([modified_phrase, ":".join(reponse)])
     return processed_data
